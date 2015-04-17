6d3d18d8bc3b0a0fdeff4f3d2f7664d5c5768ace
Jesse Trimble <jesseltrimble@gmail.com>
2015-04-16 18:45:07 -0600

    Expand console functionality

--- lib/console.rb
+++ lib/console.rb
@@ -12,3 +12,12 @@ Pry.config.prompt_name = "elio"
 def octokit_client
   @client ||= Octokit::Client.new(access_token: ENV['ELIO_GITHUB_OAUTH_TOKEN'])
 end
+
+def redis
+  @redis ||= Redis.new
+end
+
+def elio(message)
+  @router ||= Bitbot::Router.new
+  @router.route_message(Bitbot::Message.new(text: message, user_name: 'archer'))
+end
